fc4ce62ba80dddef01233cab677760099838b7d8
Andrew Creahan <CreedyNZ@users.noreply.github.com>
2021-09-27 02:36:43 +1300

    BLDC Update

--- firmware/lib/motor/default_motor.h
+++ firmware/lib/motor/default_motor.h
@@ -216,4 +216,51 @@ class ESC: public MotorInterface
         }
 };
 
+
+class BLDC: public MotorInterface
+{
+    private:
+        int in_pin_;
+        int pwm_pin_;
+
+    protected:
+        void forward(int pwm) override
+        {
+            digitalWrite(in_pin_, HIGH);
+            analogWrite(pwm_pin_, abs(pwm));
+        }
+
+        void reverse(int pwm) override
+        {
+            digitalWrite(in_pin_, LOW);
+            analogWrite(pwm_pin_, abs(pwm));
+        }
+
+    public:
+        BLDC(float pwm_frequency, int pwm_bits, bool invert, int pwm_pin, int in_pin, int unused=-1): 
+            MotorInterface(invert),
+            in_pin_(in_pin),
+            pwm_pin_(pwm_pin)
+        {
+            pinMode(in_pin_, OUTPUT);
+            pinMode(pwm_pin_, OUTPUT);
+
+            if(pwm_frequency > 0)
+            {
+                analogWriteFrequency(pwm_pin_, pwm_frequency);
+            }
+            analogWriteResolution(pwm_bits);
+
+            //ensure that the motor is in neutral state during bootup
+            analogWrite(pwm_pin_, abs(0));
+        }
+
+        void brake() override
+        {
+            analogWrite(pwm_pin_, 0);
+        }
+};
+
+
+
 #endif
